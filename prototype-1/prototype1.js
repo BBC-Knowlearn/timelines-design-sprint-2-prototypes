aaf1297075d0ebcbbc1165bd6cf3f4512597783c
eemmzz <emma.maloney@bbc.co.uk>
2015-01-23 11:10:23 +0000

    Removed emp players and hopefully fixed it so you can open multiple drawers at once

--- prototype-1/prototype1.js
+++ prototype-1/prototype1.js
@@ -1,16 +1,34 @@
 $(function() {
 	previousEvent = "";
+	drawers = [
+		{
+			"drawer1": "close"
+		},
+		{
+			"drawer2": "close"
+		},
+		{
+			"drawer3": "close"
+		},
+		{
+			"drawer4": "close"
+		}
+	];
 
 	$('.k-tl-p1-drawer--open__wrapper').hide();
 
 	$('.k-tl-p1-drawer--open__close-button').click(function(event) {
-		if (previousEvent !== "close") {
+		var buttonId = event.target.id;
+
+		if (drawers[buttonId] !== "close") {
+		//if (previousEvent !== "close") {
 			var buttonId = event.target.id;
 			var selectorForOpenDrawer = 'div#' + buttonId + ".k-tl-p1-drawer--open__wrapper";
 			var selectorForClosedDrawer = 'div#' + buttonId + ".k-tl-p1-drawer--closed";
 
 			if ($(selectorForOpenDrawer).is(":visible")) {
-				previousEvent = "close";
+				//previousEvent = "close";
+				drawers[buttonId] = "close";
 				$(selectorForOpenDrawer).toggle("slide", { direction: "right" } );
 				$(selectorForClosedDrawer).delay(200).toggle("fade");
 			}
@@ -21,16 +39,16 @@ $(function() {
 	});
 
 	$('.k-tl-p1-drawer--closed').click(function(event) {
-		if (previousEvent !== "open") {
-			var hookId = event.target.id;
+		var hookId = event.target.id;
+
+		if (drawers[hookId] !== "open") {
+		//if (previousEvent !== "open") {
 			var selectorForOpenDrawer = 'div#' + hookId + ".k-tl-p1-drawer--open__wrapper";
 			var selectorForClosedDrawer = 'div#' + hookId + ".k-tl-p1-drawer--closed";
 			var currentEventPid = $(this).data('id-pid');
 
 			// Ensure the top of the event is in view for iPad
 			var valueToScroll = $("section#" + currentEventPid).offset().top;
-			//$("html, body").scrollTop(valueToScroll - 120);
-
 			$("html, body").animate({ scrollTop: valueToScroll - 120 }, 
 			{
 				duration: 500,
@@ -38,7 +56,8 @@ $(function() {
 			});
 
 			if ($(selectorForClosedDrawer).is(":visible")) {
-				previousEvent = "open";
+				drawers[hookId] = "open";
+				//previousEvent = "open";
 				$(selectorForClosedDrawer).toggle("fade", { duration: 200 });
 				$(selectorForOpenDrawer).delay(100).toggle("slide", { direction: "right" } );
 			}
